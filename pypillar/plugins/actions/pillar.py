1d8e82f6ed449afe560fc55b7c760915b083c57b
phina <phiphiphina@gmail.com>
2018-08-15 16:49:16 +0800

    auto type

--- pypillar/plugins/actions/pillar.py
+++ pypillar/plugins/actions/pillar.py
@@ -14,7 +14,6 @@ class ActionModule(ActionBase):
             return result
 
         # parser host
-        pillar = task_vars.get('pillar', {})
         inventory_hostname = task_vars.get('inventory_hostname', '')
         ansible_distribution = task_vars.get('ansible_distribution', '')
         ansible_distribution_version = task_vars.get('ansible_distribution_version', '')
@@ -22,14 +21,28 @@ class ActionModule(ActionBase):
         facts['myhost']=myhost
         # process pillar
         pillar = task_vars.get('pillar', {})
+
+        check_osver=kwargs.get('check_osver',False)
+
         choice_map = {
             "dict": get_setting.get_dict,
             "list": get_setting.get_list,
         }     
         for k, v in pillar.items():
             self._display.v('parse setting: {}'.format(str(k)))
-            args=v.pop('args', {})
-            func_type=args.pop('type', 'dict')
+#            args=v.pop('args', {})
+#            func_type=args.pop('type', 'dict')
+            check_osver=args.get('check_osver', False)
+            if 'default' in v:
+                if check_osver:
+                    func_type=type(v['default'].values()[0]).__name__
+                else:
+                    func_type=type(v['default']).__name__
+            else:
+                if check_osver:
+                    func_type=type(v.values()[0].values()[0].values()[0]).__name__
+                else:
+                    func_type=type(v.values()[0].values()[0]).__name__
             facts[k] = choice_map.get(func_type)(myhost, v, **args)
         result['failed'] = False
         pillar.update(facts)
